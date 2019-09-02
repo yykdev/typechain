3803eadc852875438bf13ae6d79afd97867b2e6f
yykdev <assavictory@gmail.com>
2019-09-02 23:06:15 +0900

    typescript 의 class 사용 법

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "nouveau",
-    age: 33,
-    gender: "male"
-};
+const nouveau = new Human("Nouveau", 33, "male");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender} !!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(nouveau));
 
 export {};
