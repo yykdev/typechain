4d3dabcf4f6e5f99cbc26b9b3254862244ef5d45
yykdev <assavictory@gmail.com>
2019-09-02 18:01:40 +0900

    타입스크립트 사용 예

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "nouveau",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
