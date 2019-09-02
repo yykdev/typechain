da6261d4d43c42a34cf648c3524d615c9aeebdec
yykdev <assavictory@gmail.com>
2019-09-02 23:00:28 +0900

    typescript 의 interface 사용 법

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender} !!`;
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "nouveau",
+    age: 33,
+    gender: "male"
 };
 
-console.log(sayHi("nouveau", 33, "male"));
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender} !!`;
+};
+
+console.log(sayHi(person));
 
 export {};
